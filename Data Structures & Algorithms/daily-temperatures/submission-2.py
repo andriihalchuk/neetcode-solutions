e22498038104ccb1354a40f7a859c53caffbaf07
andriihalchuk <164888572+andriihalchuk@users.noreply.github.com>
2026-08-17 20:47:23 +0100

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # if the value we encounter is greater than the top of the stack then subtract indices and add to result
+        # if lesser then add to the stack
+
+        stack, res = [], [0] * len(temperatures)
+
+        for i, t in enumerate(temperatures):
+            
+            while stack and t > stack[-1][0]:
+                stackVal, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append([t, i])
+            
+            if t < stack[-1][0]:
+                stack.append([t, i])
+        
+        return res
